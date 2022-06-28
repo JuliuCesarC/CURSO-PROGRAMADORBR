3c442f9f3add65c4a289f30e1b6769c339eb41ce
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-06-28 19:59:23 -0300

    new

--- Desafio jogo da memoria/Game.js
+++ Desafio jogo da memoria/Game.js
@@ -1,28 +1,8 @@
 let game = {
-
     travaDoJogo: false,
     primeiraCarta: null,
     segundaCarta: null,
-
-    setaCarta: function(id){
-            // Cria um filtro que retorna toda carta com o mesmo id que foi selecionado, como cada carta tem um id aleatorio, sómente a carta selecionado que retornara. O resultado é o objeto da carta selecionada.
-        let card = this.cartas.filter(card => card.id === id)[0];
-
-        if(card.flipped || this.travaDoJogo){
-            return false;
-        }
-
-        if(!this.primeiraCarta){
-            this.primeiraCarta = card;
-            return true;
-        }else{
-            this.segundaCarta = card;
-            this.travaDoJogo = true;
-            return true;
-        }
-
-    },
-
+    cartas: null,
     tecnologias:[
         'bootstrap',
         'css',
@@ -35,7 +15,47 @@ let game = {
         'node',
         'react'],
 
-        cartas: null,
+    setaCarta: function(idSelecionado){
+            // Cria um filtro que retorna toda carta com o mesmo id que foi selecionado, como cada carta tem um id aleatorio, sómente a carta selecionado que retornara. O resultado é o objeto da carta selecionada.
+        let card = this.cartas.filter(card => card.id === idSelecionado)[0];
+            // O 'idSelecionado' é o id que a função 'viraCarta' enviou para aqui.
+        if(card.flipped || this.travaDoJogo){
+                //Retorna falso caso a carta selecionada ja esteja virada, impedindo de selecionador a mesma carta 2 vezes, ou de selecionar uma carta que ja tenha feito um par.
+            return false;
+        }
+
+        if(!this.primeiraCarta){
+                // Caso seja a primeira carta selecionada, a 'primeiraCarta' recebe o objeto com as informações da carta, e a opção 'flipped' vira 'true', para que a carta faça a animação de virar.
+            this.primeiraCarta = card;
+            this.primeiraCarta.flipped = true; //A função flipped tambem serve para que esta carta não possa ser selecionada novamente.
+            return true;
+        }else{
+            this.segundaCarta = card;
+            this.segundaCarta.flipped = true;
+            this.travaDoJogo = true; //Para garantir que a animação de desvirar as cartas ocorra normalmente, e 'travaDoJogo' é ativada, dessa forma impedindo de selecionar qualquer carta durante a animação.
+            return true;
+        }
+
+    },
+    verificaMatch:function(){
+        return this.primeiraCarta.icon === this.segundaCarta.icon //Compara o icone da primeira carta com o da segunda carta, caso verdadeiro retorna 'true' e o par esta formado. 
+    },
+
+    desviraCartas(){ //Esta função não ativa a animação de desvirar as cartas em si, porém ela atualiza a informação da carta.
+        this.primeiraCarta.flipped = false; //Setando o 'flipped' para 'false', faz com que a carta possa ser selecionada novamente.
+        this.segundaCarta.flipped = false;
+        this.limpaCartas(); //A função 'limpaCartas' abaixo reseta as variaveis que recebem as informações das cartas, para que elas possam receber as informações das novas cartas selecionadas, e tambem para garantir que os 'IFs' de verificações funcionem corretamente.
+    },
+    limpaCartas: function(){
+        this.primeiraCarta = null;
+        this.segundaCarta = null;
+        this.travaDoJogo = false;
+    },
+
+    verificaGameOver(){
+            // O filtro abaixo retorna um array contendo todas as cartas NÃO viradas.
+        return this.cartas.filter(cards=>!cards.flipped).length == 0; //Caso o array tenha o comprimento(length) de 0, ou seja todas as cartas formaram um par e estão viradas, então retorna 'true'. 
+    },
 
         criandoCartasComTecs:function(){
             this.cartas = [];
@@ -75,7 +95,6 @@ let game = {
                 [this.cartas[randomIndex], this.cartas[indexAtual]] = [this.cartas[indexAtual], this.cartas[randomIndex]]
             }
         }
-
 }
 // Abaixo temos uma maneira de embaralhar um array, porem ele gera um novo array com base no array original. 
 // function embaralha(array){
--- Desafio jogo da memoria/script01.js
+++ Desafio jogo da memoria/script01.js
@@ -1,53 +1,79 @@
 const FRONT = "cartaFrente"
 const BACK = "cartaAtraz"
 const CARD = "carta"
-const ICON = 'icon'
 
-mostrarCartas(game.criandoCartasComTecs())
+comecaJogo()
 
-function mostrarCartas(cartas){
+function comecaJogo(){
+    mostraCartas(game.criandoCartasComTecs());
+}
+
+function mostraCartas(){
     let tabuleiro = document.getElementById('tabuleiro')
+    tabuleiro.innerHTML = '';
 
-        // Loop com cada elemento do array 'cartas'
-    game.cartas.forEach((ctObjeto)=>{
-        let criaCarta = document.createElement('div')
-        criaCarta.id = ctObjeto.id
-        criaCarta.classList.add(CARD)
-        criaCarta.dataset.icon = ctObjeto.icon
+        // Loop com cada elemento do array 'cartas'. Cada elemento é o objeto contendo as informações de cada carta.
+        game.cartas.forEach((ctObjeto)=>{
+            let criaCarta = document.createElement('div')
+            criaCarta.id = ctObjeto.id  //A div que esta sendo criada, recebe o id que esta dentro do objeto com as informações das cartas.
+            criaCarta.classList.add(CARD)
+            criaCarta.dataset.icon = ctObjeto.icon //O dataset ira colocar o icone do objeto dentro do parametro 'data-icon' da div. Com isso poderemos verificar se as 2 cartas viradas são a mesma.
 
-        criaConteudoCartas(ctObjeto, criaCarta)
+            criaConteudoCartas(ctObjeto, criaCarta) //Envia para a função o objeto com as informações da carta, e a div que foi criada.
 
-        criaCarta.addEventListener('click', viraCarta)
-        tabuleiro.appendChild(criaCarta)
-    })
+            criaCarta.addEventListener('click', viraCarta) //Adiciona um evento de clique, que ativa a função 'viraCarta', e o elemento 'div' selecionado é enviado para essa função.
+            tabuleiro.appendChild(criaCarta) //Adiciona a div ao tabuleiro. 
+        })
 }
 function criaConteudoCartas(ctObjeto, elementoPai){
     criaFace(FRONT, ctObjeto, elementoPai);
     criaFace(BACK, ctObjeto, elementoPai);
 }
-function criaFace(face, ctObjeto, elementoPai){
-    let elementoFace = document.createElement('div')
-    elementoFace.classList.add(face)
-    if(face == FRONT){
+function criaFace(FACE, ctObjeto, elementoPai){
+    let elementoFace = document.createElement('div') //Cria uma nova div dentro da div que foi criada acima.
+    elementoFace.classList.add(FACE) //Adiciona a classe da carta, se é a frente ou a parte de traz.
+    if(FACE == FRONT){
         let elementoFilho = document.createElement('img')
-        elementoFilho.classList.add(ICON)
-        elementoFilho.src = "./images/"+ ctObjeto.icon +".png"
-        elementoFace.appendChild(elementoFilho)
+        elementoFilho.src = "./images/"+ ctObjeto.icon +".png" //Adiciona a imagem da frente da carta.
+        elementoFace.appendChild(elementoFilho) //Adiciona a imagem como filho da nova div criada.
     }else{
         elementoFace.innerHTML = "&lt/&gt"
     }
-    elementoPai.appendChild(elementoFace)
+    elementoPai.appendChild(elementoFace) //Adiciona a nova div criada como filho da div da carta, primeiro a frente, e depois a parte de traz.
 }
 function viraCarta(){
-
-    if(game.setaCarta(this.id)){
+        // A função viraCarta esta recebendo a div que contem as informações da carta, como o id, o data-icon...
+    if(game.setaCarta(this.id)){ //O 'this.id' pega o id dessa div que a função recebeu, e envia para a função 'setaCarta', e caso ela retorne 'true' executa os comandos abaixo.
         this.classList.add('flip')
-        game.verificaMatch();
+        if(game.segundaCarta){ //Somente executa os comandos abaixo caso seja a segunda carta selecionada, pois não adiante verificar o par com apenas uma carta selecionada.
+            if(game.verificaMatch()){
+                // Caso o par seja formado, a função 'verificaMatch' retorna true, que executa o comando:
+                game.limpaCartas() //O comando 'limpaCartas' ira remover as informações das cartas selecionadas da 'primeiraCarta' e 'segundaCarta', desse forma as cartas que formaram um par não poderam ser desviradas.
+                if(game.verificaGameOver()){ //E por ultimo verifica se o par formado, foi o ultimo do jogo, dessa forma aparecendo a tela de final do jogo.
+                    let gameOver = document.getElementById('gameOver')
+                    gameOver.style.display = 'flex';
+                }
+            }else{
+                setTimeout(() => { //O 'setTimeout' é para dar um delay na animação de desvirar as cartas, para dar tempo do jogador ver qual foi a segunda carta selecionada.
+                    let primeiraCartaVista = document.getElementById(game.primeiraCarta.id)
+                        // O 'game.primeiraCarta.id' ira retornar uma string com a informação do id da carta selecionada. Dessa forma o getElement ira buscar esta carta no html para poder trabalhar ela aqui dentro.
+                    let segundaCartaVista = document.getElementById(game.segundaCarta.id)
+                    
+                    primeiraCartaVista.classList.remove('flip') //O 'classList' acessa a classe da carta selecionada, assim podendo removar o 'flip', que faz a animação de desvirar a carta.
+                    segundaCartaVista.classList.remove('flip')
+                    game.desviraCartas()
+                    }, 1000);
+            };
+        }
     }
-
 }
 
-    
+function restart(){
+    game.limpaCartas();
+    comecaJogo()
+    let gameOver = document.getElementById('gameOver')
+    gameOver.style.display = 'none';
+}
 
 
 
