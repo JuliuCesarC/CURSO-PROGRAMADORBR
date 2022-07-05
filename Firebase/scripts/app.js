32e073f184df99903616785aa12ac3f8f18be639
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-07-05 14:05:31 -0300

    new

--- Firebase/scripts/app.js
+++ Firebase/scripts/app.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-analytics.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+    // Precisamos inicializar o firebase para posteriormente podermos obter alguma informação do servidor.
+firebase.initializeApp(firebaseConfig)
+
+let dataBase = firebase.firestore();
+
+// O 'collection' é referente a coleção que foi colocada no firestore, no caso a coleção 'turmaA'.
+// O get como visto anteriormente é um 'promise' que serve para pegar as informações da coleção, e como é uma promise, precisamos de um 'then' para trabalhar com as informações recebidas.
+dataBase.collection('turmaA').get().then((snapshot) => {
+    // Geralmente é utilizado como NOME do parametro da arrow function do 'then' o nome 'snapshot'. 
+    snapshot.forEach((doc)=>{ 
+    // Como a coleção é mais ou menos como um "array", então podemos utilizar um 'forEach' para executar uma função com cada item desta coleção. O elemento 'doc' que utilizamos acima será um JSON.
+        console.log(doc.data())
+    })
+})
