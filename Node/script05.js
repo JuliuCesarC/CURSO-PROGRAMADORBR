8fe198d7de4952a4dd0e2d8c99afc4671f330efb
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-08-18 17:15:25 -0300

    new

--- Node/script05.js
+++ Node/script05.js
@@ -1,26 +1,35 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
+const http = require("http");
+const url = require("url");
+// O 'url' acima recebera um objeto contendo varias funções, uma dessas funções é o método 'Url' que nos informara alguns parametros da requisição feita atravez do URL.
+const fs = require("fs");
 
-http.createServer((requisicao, resposta)=>{
-
+http
+  .createServer((requisicao, resposta) => {
     let path = url.parse(requisicao.url).pathname;
-    if(path == '' || path == '/'){
-        path = "/index.html"
+    // Como o 'url' recebe um obejeto, utilizamos o '.parse' do JSON para interpretar os dados. Passando a 'requisicao.url' junto com o '.pathname' temos o nome do caminho que foi requisitano na url.
+    // Para o exemplo apresentado, estamos abrindo uma pagina html atravez de uma requisição no servidor Node com o 'http://localhost:3000/'.
+    if (path == "" || path == "/") {
+      path = "/index.html";
+      // Caso não seja digitado nenhum caminho no Url da requisição, então atribuimos o caminho da pagina em questão dentro deste 'if'.
     }
-    let fileName = "."+path;
-   
-    fs.readFile(fileName, (erro, data)=>{
-        if(erro){
-        resposta.writeHead(404, {'Content-Type':'text/html;charset=utf-8'})
-        resposta.end('<h1>Página não encontrada</h1>')
-    }else{
-        resposta.writeHead(200, {'Content-Type':'text/html'})
-        resposta.write(data)
-        resposta.end()
-    }})
+    let fileName = "." + path;
 
-}).listen(3000, (erro)=>{
-    if(erro){console.log(erro)
-    }else{console.log("Servidor rodando na porta 3000.")}
-})
+    fs.readFile(fileName, (erro, data) => {
+      if (erro) {
+        resposta.writeHead(404, { "Content-Type": "text/html;charset=utf-8" });
+        // Caso o caminho digitado na requisição não seja de nenhuma opção, então apresentamos o código 404 de pagina não encontrada.
+        resposta.end("<h1>Página não encontrada</h1>");
+      } else {
+        resposta.writeHead(200, { "Content-Type": "text/html" });
+        resposta.write(data);
+        resposta.end();
+      }
+    });
+  })
+  .listen(3000, (erro) => {
+    if (erro) {
+      console.log(erro);
+    } else {
+      console.log("Servidor rodando na porta 3000.");
+    }
+  });
---
+++ Node/script06.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+// O script06 é o mesmo do script05, porém tratando os dados da requisição. Existem formas muito melhores de tratar esses dados, mas este método tem mais função como educativo.
+function tratandoArq(req, res, callback) {
+  // A função 'tratandoArq' esta recebendo a função 'tratandoReq' como callback, dessa forma, caso a requisição de erro, ela ira chamar a função callback.
+  let path = url.parse(req.url).pathname;
+  let fileName = "." + path;
+
+  fs.readFile(fileName, (erro, data) => {
+    if (erro) {
+      if (callback) {
+        if (!callback(req, res)) {
+          // Se após passar pela função 'tratandoReq' e nenhum tratamento de dados foi adequado, então ela retorna 'false', o que executa os 2 códigos abaixo.
+          res.writeHead(404, { "Content-Type": "text/html;charset=utf-8" });
+          res.end("<h1>Página não encontrada</h1>");
+        }
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  });
+}
+
+function tratandoReq(req, res) {
+  // Dentro dessa função podemos adicionar varias tipos de tratamento de dados, abaixo temos apenas 1 simples tipo de tratamento. 
+  let path = url.parse(req.url).pathname;
+  if (path == "/teste") {
+    res.end("Teste");
+    return true;
+  }
+  return false;
+}
+
+http
+  .createServer((requisicao, resposta) => {
+    tratandoArq(requisicao, resposta, tratandoReq);
+  })
+  .listen(3000, (erro) => {
+    if (erro) {
+      console.log(erro);
+    } else {
+      console.log("Servidor rodando na porta 3000.");
+    }
+  });
