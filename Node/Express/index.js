9089437adc906540afbb761d936c05356a17f2a5
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-08-19 19:24:41 -0300

    new

--- Node/Express/index.js
+++ Node/Express/index.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use("/teste",express.static(path.join(__dirname, "client")));
 // A cada requisição o '.use' tenta entregar alguma resposta. O mais basico é buscar por algum arquivo com o nome 'index.html' dentro da pasta 'client'.
 
 app.get("/", (req, res) => {
-    
+    console.log(req)
     // res.set("Content-Typer", "text/plain");
     // O '.set' permite alterar qualquer informação no cabeçalho do 'writeHead'. Para alterar apenas o 'content-type' utilizamos o :
-    res.type("txt");
+    res.type("html");
     // Por padrão o content-type do express já vem como "text/html", então só alteramos se for necessario outro tipo de interpretação.
     // .type("html"): PADRÃO texto e html
     // .type("txt"): text/plain
