92dce96a4767e133906bc4a9c739ff3cf52af646
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-08-22 19:05:39 -0300

    new

--- Node/Express/index02.js
+++ Node/Express/index02.js
@@ -1,39 +1,39 @@
 const express = require("express");
 const app = express();
-const PORT = 5000
-const bodyP = require("body-parser")
+const PORT = 5000;
 
 // A caracteristica do Middleware é ter acesso ao 'req, res' e a um parâmetro 'next'.
 // app.get("/", (req, res)=>{
 //     res.send("Hello World")})
 // app.post("/", (req, res)=>{
-//     res.send("Hello World")}) 
+//     res.send("Hello World")})
 // Como o get e o post estão executando o mesmo trabalho, podemos pegar esse Middleware e colocar em uma função, passando essa função para os 2.
 
-let consMetodo = (req, res, next)=>{
+let consMetodo = (req, res, next) => {
     // Como esse Middleware não possui resposta, o programa fica travado nessa função, e não segue para o proximo Middleware.
-    console.log(req.method)
-    next()
-    // Para isso utilizamos o 'next()', que fara o programa seguir após executar essa função. 
+    console.log(req.method);
+    next();
+    // Para isso utilizamos o 'next()', que fara o programa seguir após executar essa função.
     // Dentro de um Middleware é possivel executar varias coisas, e caso seja necessario parar o programa caso aconteça algum erro, podemos utilizar o 'next' também, só é preciso passar algum parametro para ele:
     // next("[erro] Não foi possivel executar o ....")
-}
-let consBody = (req, res, next)=>{
+};
+let consBody = (req, res, next) => {
     console.log(req.body);
-    next()
-}
-let hello = (req, res)=>{
-    res.send("Hello World")
-}
+    next();
+};
+let hello = (req, res) => {
+    res.send("Hello World");
+};
 
-app.use(express.urlencoded({extended:true}))
-app.use("/", consMetodo, consBody)
+app.use(express.json());
+// Com o express mais atual, não é necessario instalar o body-parser, pois ele ja é uma dependencia do express. E para esse novo método utilizamos o 'express.json()' e o 'express.urlencoded()', dessa forma podemos pegar as informações inviadas no body da requisição.
+app.use(express.urlencoded({ extended: true }));
+app.use("/", consMetodo, consBody);
 // Podemos também utilizar o 'use' com um Middleware, dessa forma todo resquest, independente de qual tipo(get, post, put...) sempre passara por esse Middleware em questão.
 
-app.get("/", /* consMetodo */ hello)
+app.get("/", /* consMetodo */ hello);
 // É possivel passar varios Middleware como parametro. Mas como estamos utilizando o '.use' acima, não é necessario passar o 'consMetodo' aqui.
-app.post("/", /* consMetodo */ hello)
-
+app.post("/", /* consMetodo */ hello);
 
 app.listen(PORT, () => {
     console.log("Rodando na porta 5000");
---
+++ Node/Express/index03.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+app.use(express.urlencoded({ extended: true }));
+
+let alunos = [
+    { id: 0, nome: "Jão" },
+    { id: 1, nome: "NataN" },
+    { id: 2, nome: "Jenfiner" },
+    { id: 3, nome: "Lusca" },
+];
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.get("/Alunos", (req, res) => {
+    res.json(JSON.stringify(alunos));
+});
+
+app.get("/aluno", (req, res, next) => {
+    console.log(req.body);
+    let aluno = alunos[req.body.id];
+    // Com o body-parse dentro do express, podemos pegar informações do body da requisição. 
+    // Url utilizado na requisição: http://localhost:5000/aluno
+    res.json(aluno);
+    // Neste exemplo, é enviado o id do aluno na requisição atravez de um formulario, então é buscado dentro do array alunos, e retornado o aluno selecionado.
+    next()
+});
+app.get("/aluno", (req, res)=>{
+    let alunoQuery = alunos[req.query.id]
+    console.log("-----")
+    console.log(alunoQuery)
+    console.log("-----")
+    res.json(alunoQuery)
+})
+app.get("/aluno/:id", (req, res) => {
+    // Podemos tambem criar um rota especifica para enviar uma informação na requisição atravez do URL.
+    // Url utilizado na requisição: http://localhost:5000/aluno/1
+    console.log(req.params.id);
+    let aluno = alunos[req.params.id];
+    res.json(aluno);
+    // E para selecionar o aluno com informações do url, é utilizado o 'params.'.
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log("Rodando na porta 5000");
+});
