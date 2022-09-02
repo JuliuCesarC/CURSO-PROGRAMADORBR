1f958b16176a14d7b16778a2147eb38a86a7841b
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-09-02 11:20:23 -0300

    new

--- MongoDB/login jwt/app01.js
+++ MongoDB/login jwt/app01.js
@@ -2,6 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRouter");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Conectado no MongoDB");
+    }
+});
 
 app.use("/user", userRouter);
 
