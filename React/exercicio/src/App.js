c90d9dc36cbc4aa70e30cefd469c1feda61c36f3
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-09-16 18:25:01 -0300

    new

--- React/exercicio/src/App.js
+++ React/exercicio/src/App.js
@@ -4,9 +4,10 @@ import Header from './Header'
 // A função 'App' esta sendo importada dentro do arquivo index.js, onde os elementos que estamos criando abaixo seram renderizados lá.
 function App() {
   // Dentro da função 'App' também estamos importando outras funções, como o arquivo 'header.js' que fica responsavel pelo cabeçalho da pagina.
-  // Podemos importar varias funções, cada arquivo funcionando como um módulo, responsavel por 1 ou mais elementos.
+  // Podemos importar varias funções, cada arquivo funcionando como um módulo, e esse por sua vez responsavel por 1 ou mais elementos.
   return (
-    <Header title='React' links = {['Documentação', 'Produtos', 'Contato']}></Header>
+    // Como nós preparamos o arquivo 'Header.js' para receber as informações que serão exibinas no cabeçalho, precisamos enviar elas atravez das propriedades da ta 'Header'
+    <Header title='React' links = {['Documentação', 'Produtos', 'Contato', 'Login']}></Header>
   );
 }
 
---
+++ React/exercicio/src/Menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Menu(props){
+    return (
+    <ul className="menu">
+      {props.links.map((link, index)=><li key={index}>{link}</li>)}
+    </ul>)
+}
+
+export default Menu
