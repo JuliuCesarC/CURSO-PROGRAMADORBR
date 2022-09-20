0f099d218609b9b7e7ab0cdb4a84a2ad29d86a08
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-09-20 11:17:15 -0300

    new

--- React/Hooks/src/Counter.js
+++ React/Hooks/src/Counter.js
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// Para utilizar os hooks precisamos importar eles, facilmente são identificados por começar com o 'use...'. Temos o 'useState, useEffect, useContext' entre outros. Para uma descrição detalhada de cada Hooks, pesquisar na documentação:https://reactjs.org/docs/hooks-state.html ou https://www.w3schools.com/react/react_hooks.asp.
 
-class Counter extends React.Component {
+// Os 'Hooks' são utilizadas em funções, nos permitindo ter acesso ao estado de variaveis e as funcionalidades do React, como os ciclos de vida. Essas funcionalidades reduzem consideravelmente a quantidade de código necessarios para executar algo, comparado com as mesmas funções utilizando Classes.
+function Counter(props) {
+    // Para ter acesso ao estado de uma variavel com a classe, era apenas necessario iniciar ela com um 'this.state =...', porem como estamos em uma função, não herdamos nenhum método. Precisamos então declarar uma variavel utilizando os hooks.
+    const [contador, setContador] = useState(2);
+    // O 'useState' retorna um array com 2 índices, o primeiro é o estado inicial da variavel, e esse estado é definido no parâmetro do 'useState', já o segundo é uma função que nos permite atualizar o valor do estado da variavel.
+    // A partir do ES6 podemos fazer uma desestruturação na hora de declarar uma variavel, ela pode ser feita utilizando vetor[] ou um objeto{}, no nosso caso utilizamos o vetor. Por consenso a segunda variavel tem o mesmo nome da primeira, porem com o 'set' na frente.
 
-    constructor(props) {
-        super(props);
+    useEffect(() => {
+        setContador(parseInt(localStorage.getItem("count")));
+    },[]);
 
-        this.state = { count: this.props.count };
-        this.add = this.add.bind(this);
+    useEffect(() => {
+        document.title = `Contador: ${contador}`;
+        localStorage.setItem("count", contador);
+    }, [contador]);
+
+    function add() {
+        setContador(contador + 1);
+        // Com o 'setContador' atualizamos o valor da variavel.
     }
-    
-    add() { 
-        this.setState((state) => {
-            return { count: state.count + 1 };
-        });
-    }
-    render() {
-        return (
-            <div>
-                <h1>Counter : {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>Counter : {contador}</h1>
+            <button onClick={add}>Add</button>
+        </div>
+    );
 }
 
 export default Counter;
