fcb5c1a36b6b73d5f691845b6a5f466e99cc2add
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-09-21 12:12:45 -0300

    new

---
+++ React/Classes/Vite/src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Card(){
+    return(
+        <div className='card'>
+            <strong>Juliu Cesar</strong>
+            <small>11:04</small>            
+        </div>
+    )
+}
+
+export default Card
--- React/Classes/Vite/src/pages/Home/index.jsx
+++ React/Classes/Vite/src/pages/Home/index.jsx
@@ -1,11 +1,25 @@
 import "./styles.css";
+import Card from "../../components/Card";
+import { useState } from "react";
 
 function Home() {
+    const [guestName, setGuestName] = useState()
     return (
-        <div>
+        <div className="container">
             <h1>Lista de Presença</h1>
-            <input type="text" placeholder="Digite o nome..." />
+            <h4>{guestName}</h4>
+            <input
+                type="text"
+                placeholder="Digite o nome..."
+                onChange={(e) => {
+                    setGuestName(e.target.value);
+                }}
+            />
             <button type="button">Adicionar</button>
+
+            <Card />
+            <Card />
+            <Card />
         </div>
     );
 }
