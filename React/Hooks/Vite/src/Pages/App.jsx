a5f6af26f40e2f32a765a350e603eee42ec382df
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-09-23 12:15:03 -0300

    new

--- React/Hooks/Vite/src/Pages/App.jsx
+++ React/Hooks/Vite/src/Pages/App.jsx
@@ -1,18 +1,40 @@
-import Card from '../components/card'
+import { useState } from "react";
+import Card from "../components/card";
 import "./App.css";
 
 function App() {
+    const [nameTask, setNameTask] = useState('');
+    const [taskGroup, setTaskGroup] = useState([{Name:'Teste'},{Name:'segundo'}])
+    function addTask() {
+      const task = {
+        name: nameTask,
+        checked: false,
+        id: "",
+      }
+      setTaskGroup((prevTask)=>{
+        return{...prevTask, }
+      })
+      console.log(taskGroup)
+    }
     return (
         <div className="container">
             <header>
                 <h1>ToDo List</h1>
             </header>
-            <form className="Add" action="/">
-                <input type="text" name="" id="" placeholder="Tarefas..." />
-                <button type="submit">Adicionar</button>
-            </form>
+            <div className="Add" action="">
+                <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Tarefas..."
+                    onChange={(e) => {
+                        setNameTask(e.target.value);
+                    }}
+                />
+                <button type="submit" onClick={addTask}>Adicionar</button>
+            </div>
             <div className="cards">
-              <Card></Card>
+                <Card></Card>
             </div>
         </div>
     );
