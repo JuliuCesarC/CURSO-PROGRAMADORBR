8451aa5475627dca9dab0f4fbc71f54f32f29b3a
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-10-13 09:47:29 -0300

    new

--- React/Hooks/Vite/teste.js
+++
@@ -1,6 +0,0 @@
-let data = new Date(
-    new Date().getFullYear(),
-    new Date().getMonth() + 1,
-    0
-).getDate();
-console.log(data);
--- React/Hooks/ViteHooks/src/main.jsx
+++ React/Hooks/ViteHooks/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Pages/App'
-import './style/index.css'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.Suspense>
+  <React.StrictMode>
     <App />
-  </React.Suspense>
+  </React.StrictMode>
 )
