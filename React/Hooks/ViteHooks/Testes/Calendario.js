6ebb14465d18371c7251be01fb90f3435df2874f
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-10-20 16:08:48 -0300

    new

--- React/Hooks/ViteHooks/Testes/Calendario.js
+++ React/Hooks/ViteHooks/Testes/Calendario.js
@@ -1,8 +1,9 @@
-// FUNÇÃO PARA INSERIR OS DIAS NA TABELA
 let Days = document.getElementById("days");
+// FUNÇÃO PARA INSERIR OS DIAS NA TABELA
 function showDays(month, year, months) {
 	// CONFIGURA O MÊS E O ANO NA TABELA
 	document.getElementById("month").innerHTML = months[month].month;
+	document.getElementById("Header").classList.add(month)
 	document.getElementById("year").innerHTML = year;
 	Days.dataset.month = month;
 	Days.dataset.year = year;
@@ -18,6 +19,7 @@ function showDays(month, year, months) {
 
 		let td = document.createElement("td");
 		td.innerHTML = indexDay.getDate();
+		td.classList.add('c'+indexDay.getDate())
 		td.addEventListener("click", (e) => {
 			showTasks(e.target);
 		});
@@ -26,7 +28,7 @@ function showDays(month, year, months) {
 			indexDay.getMonth() == Now.getMonth() &&
 			indexDay.getDate() == Now.getDate()
 		) {
-			td.classList.add("current-day");
+			td.id = "current-day";
 		}
 		if (i < 1) {
 			td.classList.add("prev-month");
@@ -41,33 +43,47 @@ function showDays(month, year, months) {
 		}
 	}
 	Days.appendChild(tr);
+
+	if(months[month].listOfAllTasks){
+		months[month].listOfAllTasks.forEach(tk=>{
+			if(tk.year==year){
+				let element = document.querySelector(`.c${tk.day}`)
+				if(element.classList.length < 2){
+					element.classList.add('task');
+				}else{
+					document.querySelectorAll(`.c${tk.day}`)[1].classList.add('task')
+				}
+			}
+		})
+	}
+	
 }
 	let Now = new Date();
 	let month = Now.getMonth();
 	let year = Now.getFullYear();
 	
 	document.getElementById("Btn-Next").onclick = function () {
+		clearTable();
 		month++;
 		if (month > 11) {
 			month = 0;
 			year++;
 		}
-		clearTable();
 		showDays(month, year, JSON.parse(localStorage.getItem("ToDoList")));
 	};
 	document.getElementById("Btn-Prev").onclick = function () {
+		clearTable();
 		month--;
 		if (month < 0) {
 			month = 11;
 		year--;
 	}
-	clearTable();
 	showDays(month, year, JSON.parse(localStorage.getItem("ToDoList")));
 };
 // LIMPA A TABELA QUANDO MUDA O MÊS
 function clearTable() {
 	let allTr = Array.from(document.querySelectorAll("tr"));
-	console.log(allTr);
+	document.getElementById("Header").classList.remove(month)
 	allTr.slice(1, 7).forEach((el) => {
 		Days.removeChild(el);
 	});
