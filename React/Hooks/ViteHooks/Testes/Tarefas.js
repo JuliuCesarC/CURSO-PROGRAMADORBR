ed58899b289669fa44782e37e4338f260d3fb3e0
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-10-21 11:51:28 -0300

    new

--- React/Hooks/ViteHooks/Testes/Tarefas.js
+++ React/Hooks/ViteHooks/Testes/Tarefas.js
@@ -3,38 +3,58 @@ const imgCheck = "../public/img/iconeCheck.png";
 
 function clearTasks() {
 	let allTk = Array.from(
-		document.getElementById("ToDo").getElementsByTagName("div")
+		document.getElementById("taskTable").getElementsByTagName("tr")
 	);
 	allTk.forEach((el) => {
-		document.getElementById("ToDo").removeChild(el);
+		document.getElementById("taskTable").removeChild(el);
 	});
 }
 clearTasks();
 
 function showTasks(month, year, day) {
 	clearTasks();
-	if(month == null && year == null){
-		month = Number(document.getElementById('Header').classList[0]);
-		year = Number(document.getElementById('year').innerHTML)
-		day = Number(day.innerHTML)
+	if (month == null && year == null) {
+		month = Number(document.getElementById("Header").classList[0]);
+		year = Number(document.getElementById("year").innerHTML);
+		day = Number(day.innerHTML);
 	}
 
 	document.getElementById("Day").innerHTML = "Dia " + day + "/" + (month + 1);
 	let monthsOfYear = JSON.parse(localStorage.getItem("ToDoList"))[month]
 		.listOfAllTasks;
-	
-	if(monthsOfYear){
-		monthsOfYear.forEach(e=>{
-			if(e.day == day){
-				e.tasks.forEach(tk=>{
-					addTask(tk)
-				})
+	if (monthsOfYear) {
+		monthsOfYear.forEach((e) => {
+			if (e.day == day) {
+				e.tasks.forEach((tk) => {
+					addTask(tk);
+				});
 			}
-		})
+		});
 	}
 
 	function addTask(tk) {
-		console.log(tk);
+		let taskTable = document.getElementById("taskTable");
+		let Tr = document.createElement("tr");
+		let workingTd = document.createElement("td");
+		let workingImg = document.createElement("img");
+		let taskCont = document.createElement("td");
+
+		if (tk.check == "check") {
+			workingImg.setAttribute("src", imgCheck);
+		} else {
+			workingImg.setAttribute("src", imgWorking);
+		}
+		workingImg.classList.add(tk.check);
+		workingImg.addEventListener("click", switchCheck);
+		workingTd.classList.add("workingTd");
+		taskCont.innerHTML = tk.cont;
+		taskCont.addEventListener("dblclick", openEditMode);
+		taskCont.classList.add("content");
+
+		workingTd.appendChild(workingImg);
+		Tr.appendChild(workingTd);
+		Tr.appendChild(taskCont);
+		taskTable.appendChild(Tr);
 	}
 
 	function switchCheck(e) {
@@ -49,6 +69,30 @@ function showTasks(month, year, day) {
 			eC.setAttribute("src", imgWorking);
 		}
 	}
+	function openEditMode(tkx) {
+		let contTask = tkx.target.innerHTML
+		tkx.target.innerHTML =''
+
+		inputTx = document.createElement('input')
+		inputBtn = document.createElement('input')
+		inputTx.setAttribute('type', 'text')
+		inputBtn.setAttribute('type', 'button')
+		inputTx.classList.add('inputTx')
+		inputBtn.classList.add('inputBtn')
+		inputTx.value = contTask
+		inputBtn.value = 'Enviar'
+		inputBtn.addEventListener('click', updateTask(tkx))
+		
+		tkx.target.appendChild(inputTx)
+		tkx.target.appendChild(inputBtn)
+		inputTx.focus()
+
+	}
+	function updateTask(tkx){
+		console.log('entrou', tkx.target);
+		return;
+	}
+
 	function addEmptyTr() {
 		let emptyTr = document.createElement("tr");
 		let workingTd = document.createElement("td");
