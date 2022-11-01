aef6c462ec82643495ed0fdf64df32cf27c2dd9a
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-11-01 17:31:35 -0300

    new

--- Exercicio Chat/index02.js
+++ Exercicio Chat/index02.js
@@ -18,23 +18,19 @@ const io = socketio(Server);
 const grupo1 = io.of("/sala1").on("connection", (socket) => {
     // O '.of' permite criar um grupo onde somente os navegadores conectados nesse grupo seram atualizados com o '.on'.
     socket.emit("update_messages", messages.sala1);
-    console.log("New connection.");
 
     socket.on("new_message", (data) => {
         messages.sala1.push(data);
         grupo1.emit("update_messages", messages.sala1);
-        console.log(messages.grupo1);
     });
 });
 const grupo2 = io.of("/sala2").on("connection", (socket) => {
     // O '.of' permite criar um grupo onde somente os navegadores conectados nesse grupo seram atualizados com o '.on'.
     socket.emit("update_messages", messages.sala2);
-    console.log("New connection.");
 
     socket.on("new_message", (data) => {
         messages.sala2.push(data);
         grupo2.emit("update_messages", messages.sala2);
-        console.log(messages);
     });
 });
 
