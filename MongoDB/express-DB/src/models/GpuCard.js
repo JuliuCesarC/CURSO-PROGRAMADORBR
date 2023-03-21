06c7509256b5bbf4222441cac681959c99d8ab6d
Juliu Cesar <juliu2012@gmail.com>
2023-03-21 15:32:17 -0300

    finished express DB

--- MongoDB/express-DB/src/models/GpuCard.js
+++ MongoDB/express-DB/src/models/GpuCard.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 
 const GpuCardSchema = new mongoose.Schema({
-    id: { type: String, require: true },
-    title: { type: String, require: true },
-    img: { type: String, require: true },
-    brand: { type: String, require: true },
-    memory: { type: Number, require: true },
-    ray: { type: Boolean, require: true },
-    price: { type: Number, require: true },
-    rating: { type: Number, require: true },
-    relevance: { type: Number, require: true },
-    amount: { type: Number},
+  id: { type: mongoose.Schema.Types.ObjectId, ref: "products", require: true },
+  title: { type: String, require: true },
+  img: { type: String, require: true },
+  brand: { type: String, require: true },
+  memory: { type: Number, require: true },
+  ray: { type: Boolean, require: true },
+  price: { type: Number, require: true },
+  rating: { type: Number, require: true },
+  relevance: { type: Number, require: true },
+  amount: { type: Number },
 });
 
-const GpuCard = mongoose.model("gpu-cards", GpuCardSchema)
+const GpuCard = mongoose.model("gpu-cards", GpuCardSchema);
 
-export default GpuCard
+export default GpuCard;
